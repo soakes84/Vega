6d502fe348f05c7d1b7460d9171ca88e51c2b1aa
Spencer Oakes <spencer.oakes84@gmail.com>
2018-06-01 13:22:06 -0400

    created repository

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -37,9 +37,16 @@ namespace vega.Controllers
             vehicle.LastUpdate = DateTime.Now;
 
             context.Vehicles.Add(vehicle);
-            await context.SaveChangesAsync();
+            await context.SaveChangesAsync();   
 
-            var result = mapper.Map<Vehicle, SaveVehicleResource>(vehicle);
+            vehicle = await context.Vehicles
+            .Include(v => v.Features)
+                .ThenInclude(vf => vf.Feature)
+            .Include(v => v.Model)
+                .ThenInclude(m => m.Make) 
+            .SingleOrDefaultAsync(v => v.Id == vehicle.Id);         
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
             return Ok(result );
         }
         [HttpPut("{id}")]
@@ -55,7 +62,12 @@ namespace vega.Controllers
                 return BadRequest(ModelState);
             }
 
-            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+            var vehicle = await context.Vehicles
+            .Include(v => v.Features)
+                .ThenInclude(vf => vf.Feature)
+            .Include(v => v.Model)
+                .ThenInclude(m => m.Make) 
+            .SingleOrDefaultAsync(v => v.Id == id);
 
             if(vehicle == null)
             return NotFound();
@@ -65,7 +77,7 @@ namespace vega.Controllers
 
             await context.SaveChangesAsync();
 
-            var result = mapper.Map<Vehicle, SaveVehicleResource>(vehicle);
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
             return Ok(result );
         }
 
