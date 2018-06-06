1cc09198c91b1c6876e4f8eb6c849cf77d6a232b
Spencer Oakes <spencer.oakes84@gmail.com>
2018-06-06 07:34:07 -0400

    update and delete

--- ClientApp/app/services/vehicle.service.ts
+++ ClientApp/app/services/vehicle.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { SaveVehicle } from '../models/vehicle';
 
 @Injectable()
 export class VehicleService {
@@ -25,6 +26,16 @@ export class VehicleService {
   getVehicle(id) {
     return this.http.get('/api/vehicles/' + id)
       .map(res => res.json()); 
+  }
+
+  update(vehicle: SaveVehicle) {
+    return this.http.put('/api/vehicles/' + vehicle.id, vehicle)
+      .map(res => res.json());
+  }
+
+  delete(id) {
+    return this.http.delete('/api/vehicles/' + id)
+      .map(res => res.json());
   }
 
 }
