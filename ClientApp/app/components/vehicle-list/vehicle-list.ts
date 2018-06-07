2a9d4101d3d709fed4c0c2fca6b935bb59e7a189
Spencer Oakes <spencer.oakes84@gmail.com>
2018-06-07 17:28:47 -0400

    client-side filtering

---
+++ ClientApp/app/components/vehicle-list/vehicle-list.ts
@@ -0,0 +1,41 @@
+import { Vehicle, KeyValuePair } from './../../models/vehicle';
+import { VehicleService } from './../../services/vehicle.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    templateUrl: 'vehicle-list.html'
+})
+
+export class VehicleListComponent implements OnInit {
+    vehicles: Vehicle[];
+    allVehicles: Vehicle[];
+    makes: KeyValuePair[];
+    filter: any = {};
+
+    constructor(private vehicleService: VehicleService) { }
+
+    ngOnInit() {
+        this.vehicleService.getMakes()
+            .subscribe(makes => this.makes = makes);
+            
+        this.vehicleService.getVehicles()
+            .subscribe(vehicles => this.vehicles = this.allVehicles = vehicles);
+    }
+
+    onFilterChange() {
+        var vehicles = this.allVehicles;
+
+        if(this.filter.makeId)
+            vehicles = vehicles.filter(v => v.make.id == this.filter.makeId)
+
+        if(this.filter.modelId)
+            vehicles = vehicles.filter(v => v.model.id == this.filter.modelId)
+
+        this.vehicles = vehicles;
+    }
+
+    resetFilter() {
+        this.filter = {};
+        this.onFilterChange();
+    }
+}
