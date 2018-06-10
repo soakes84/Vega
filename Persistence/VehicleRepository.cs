1f8bdbfdfd776fff5397f6d3f4c2be26aa6a0349
Spencer Oakes <spencer.oakes84@gmail.com>
2018-06-09 23:28:25 -0400

    using dictionary

--- Persistence/VehicleRepository.cs
+++ Persistence/VehicleRepository.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using vega.Core.Models;
 using vega.Core;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace vega.Persistence
 {
@@ -52,19 +54,20 @@ namespace vega.Persistence
             if (queryObj.ModelId.HasValue)
                 query = query.Where(v => v.ModelId == queryObj.ModelId.Value);
 
-            if (queryObj.SortBy == "make")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.Model.Make.Name) : query.OrderByDescending(v => v.Model.Make.Name);
+        var columnsMap = new Dictionary<string, Expression<Func<Vehicle, object>>>()
+        {
+            ["make"] = v => v.Model.Make.Name,
+            ["model"] = v => v.Model.Name,
+            ["contactName"] = v => v.ContactName,
+            ["id"] = v => v.Id
+        };
 
-            if (queryObj.SortBy == "model")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.Model.Name) : query.OrderByDescending(v => v.Model.Name);
-
-            if (queryObj.SortBy == "contactName")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.ContactName) : query.OrderByDescending(v => v.ContactName);
-
-            if (queryObj.SortBy == "id")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.Id) : query.OrderByDescending(v => v.Id);
+        if (queryObj.IsSortAscending)
+            query = query.OrderBy(columnsMap[queryObj.SortBy]);
+        else
+            query = query.OrderByDescending(columnsMap[queryObj.SortBy]);
             
-            return await query.ToListAsync();
+        return await query.ToListAsync();
         }
       
     }
