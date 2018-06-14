46b5d74d1323ec2383715d50a65c1f23b8c86ddf
Spencer Oakes <spencer.oakes84@gmail.com>
2018-06-13 21:34:08 -0400

    edge case

--- ClientApp/app/components/vehicle-list/vehicle-list.ts
+++ ClientApp/app/components/vehicle-list/vehicle-list.ts
@@ -7,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class VehicleListComponent implements OnInit {
+    private readonly PAGE_SIZE = 3;
+
     queryResult: any = {};
     makes: KeyValuePair[];
     query: any = {
-        pageSize = 3
+        pageSize = this.PAGE_SIZE
     };
     columns = [
         {title: 'Id', key:}
@@ -34,12 +36,16 @@ export class VehicleListComponent implements OnInit {
     }
 
     onFilterChange() {
+      this.query.page = 1;  
       this.populateVehicles();
     }
 
     resetFilter() {
-        this.filter = {};
-        this.onFilterChange();
+        this.query = {
+            page: 1,
+            pageSize: this.Page_Size
+        };
+        this.populateVehicles();
     }
 
     sortBy(columnName) {
