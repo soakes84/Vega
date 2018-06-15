85acb9d5f9dd6101a30e01b0e51634e82070031d
Spencer Oakes <spencer.oakes84@gmail.com>
2018-06-15 19:57:06 -0400

    Photo API

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -11,6 +11,7 @@ namespace vega.Mapping
         public MappingProfile()
         {
             // Domain to API Resource
+            CreateMap<Photo, PhotoResource>();
             CreateMap(typeof(QueryResult<>), typeof(QueryResult<>));
             CreateMap<Make, MakeResource>();
             CreateMap<Make, KeyValuePairResource>();
